cc3c36a47ca40abe36c67a2462fdbd4c841621ee
Piotr <piotrek@debian>
2017-04-18 11:06:41 +0200

    dodane wysylanie pliku na serwer, bez kontroli sumy kontrolnej

--- InfoSmieciLasyMain.py
+++ InfoSmieciLasyMain.py
@@ -1,6 +1,12 @@
-from flask import Flask, jsonify, abort, make_response, request
+from flask import Flask, jsonify, abort, make_response, request, send_file, redirect, url_for
+from werkzeug.utils import secure_filename
 import sqlite3
 import hashlib
+import os
+
+
+UPLOAD_FOLDER = '/home/piotrek/py'
+ALLOWED_EXTENSIONS  =set(['txt','pdf','png','jpg','jpeg','gif'])
 
 conn = sqlite3.connect('InfoSmieciLasyDB.db')
 app = Flask(__name__)
@@ -61,7 +67,42 @@ def get_dodaj():
     return jsonify({"wynik:":"ok"})
   #else:
     #return jsonify({"sieczkaMoja: ":sieczkaMoja})
-	
+
+
+@app.route('/file')
+def getfile():
+  print('Hello file')
+  return send_file('README.md')
+
+def allowed_file(filename):
+  return '.' in filename and filename.rsplit('.',1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/fileput', methods=['GET','POST'])
+def upload_file():
+  if request.method == 'POST':
+    if 'file' not in request.files:
+      flash('No file part')
+      return redirect(request.url)
+    file = request.files['file']
+    if file.filename == '':
+      flash('No selected file')
+      return redirect(request.url)
+    if file and allowed_file(file.filename):
+      filename = secure_filename(file.filename)
+      file.save( os.path.join(app.config['UPLOAD_FOLDER'], filename))
+#      return redirect(url_for('uploaded_file', filename = filename))
+      print('ok')
+  return '''
+  <!doctype html>
+  <title>test</title>
+  <h1>Upload new file</h1>
+  <form method=post enctype=multipart/form-data>
+  <p><input type=file name=file>
+  <input type=submit value=Upload>
+  </form>
+  '''
+
+
 
 @app.errorhandler(404)
 def not_found(error):
@@ -69,6 +110,12 @@ def not_found(error):
 
 
 
+
+
+
+
+#app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', debug=True)
